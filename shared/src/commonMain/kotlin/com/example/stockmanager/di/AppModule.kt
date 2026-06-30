2bc924b364a836ec535a5347545a58149890944d
nlizarraga9 <nicolaslizarraga152@gmail.com>
2026-06-29 23:24:14 -0300

    feat: dependencias en AppModule

--- shared/src/commonMain/kotlin/com/example/stockmanager/di/AppModule.kt
+++ shared/src/commonMain/kotlin/com/example/stockmanager/di/AppModule.kt
@@ -1,14 +1,28 @@
 package com.example.stockmanager.di
 
 import com.example.stockmanager.data.repository.ProductoRepositoryImpl
+import com.example.stockmanager.data.repository.VentaRepositoryImpl
 import com.example.stockmanager.domain.repository.ProductoRepository
+import com.example.stockmanager.domain.repository.VentaRepository
+import com.example.stockmanager.domain.usecase.CrearVentaUseCase
 import com.example.stockmanager.domain.usecase.GetProductosUseCase
+import com.example.stockmanager.presentation.productos.form.ProductoFormViewModel
 import com.example.stockmanager.presentation.productos.lista.ProductoListViewModel
+import com.example.stockmanager.presentation.venta.VentaViewModel
 import org.koin.dsl.module
 
 val appModule =
     module {
+        // Repositories
         single<ProductoRepository> { ProductoRepositoryImpl() }
+        single<VentaRepository> { VentaRepositoryImpl() }
+
+        // UseCases
         factory { GetProductosUseCase(get()) }
+        factory { CrearVentaUseCase(ventaRepository = get(), productoRepository = get()) }
+
+        // ViewModels
         factory { ProductoListViewModel(get()) }
+        factory { ProductoFormViewModel(get()) }
+        factory { VentaViewModel(getProductos = get(), crearVenta = get()) }
     }
